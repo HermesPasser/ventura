1aefa31a4a9fdc8e443d747478fe2872fafd11c6
Hermes Passer <dsldouglas@hotmail.com>
2018-06-04 20:49:42 -0300

    incremented version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name = 'ventura',
-      version = '0.1',
+      version = '0.2',
       description = 'Simple xml-based updater in python 3.6 with no try-catch statement',
       url = 'http://github.com/hermespasser/ventura',
       author = 'Hermes Passer',
