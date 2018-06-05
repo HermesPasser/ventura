d46c2f8d1b7fd715faf5c4edcf487eba1f68f4d0
Hermes Passer <dsldouglas@hotmail.com>
2018-06-04 21:29:18 -0300

    added option to extract file in subfolder

--- ventura/ventura.py
+++ ventura/ventura.py
@@ -5,7 +5,7 @@ import ventura._hpath as hpath
 
 __author__ = 'Hermes Passer'
 
-def update_if_is_need(version, xml_url):
+def update_if_is_need(version, xml_url, path = ''):
 	# download and read xml
 	xml_text = hweb.get_page(xml_url)
 	
@@ -15,8 +15,9 @@ def update_if_is_need(version, xml_url):
 	
 	links = ventura.get_links()
 	for link in links:
-		hweb.download(link, 'update.zip')
-		hzip.extract('update.zip', '')
+		path = path + '//' if path != '' else ''
+		hweb.download(link, path + 'update.zip')
+		hzip.extract(path + 'update.zip', path)
 	
 	hpath.delete_files(ventura.get_files_to_delete())
 	return True
